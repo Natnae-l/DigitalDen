bd2f25b81c8e01b149ca4201d18217d83fef27ac
Natnael Teklemariam <131717014+Natnae-l@users.noreply.github.com>
2024-02-22 08:52:43 +0300

    category component added

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,16 +1,33 @@
+import { Category } from "@mui/icons-material";
 import { Grid, Typography } from "@mui/material";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 
+const useStyles = () => {
+  return {
+    img: {
+      width: "35px",
+    },
+    shop: {
+      fontWeight: 600,
+      color: "#1b1e10",
+      fontFamily: "Anta, sans-serif !important",
+    },
+  };
+};
+
 export default function Nav() {
+  const styles = useStyles();
+
   return (
     <AppBar position="static" color="inherit" elevation={0}>
       <Toolbar>
         <Grid container alignItems="center" gap={1}>
-          <img src="/shopping-cart.png" />
-          <Typography variant="h4" color="primary">
+          <img src="/shopping-cart.png" style={styles.img} />
+          <Typography variant="h4" sx={styles.shop} fontSize={28}>
             ShopCart
           </Typography>
+          <Category />
         </Grid>
       </Toolbar>
     </AppBar>
