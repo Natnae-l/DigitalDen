c6a86dcae327101563887a13daebbd292566f8ba
Natnael Teklemariam <131717014+Natnae-l@users.noreply.github.com>
2024-02-25 16:20:09 +0300

    loader function for the home page added

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,12 @@ import {
 } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home";
+import homeLoader from "./loaders/homeLoader";
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route path="/" element={<Home />}></Route>)
+  createRoutesFromElements(
+    <Route path="/" element={<Home />} loader={homeLoader}></Route>
+  )
 );
 
 function App() {
---
+++ src/loaders/homeLoader.tsx
@@ -0,0 +1,23 @@
+interface headPhone {
+  id: string;
+  name: string;
+  description: string;
+  category: string;
+  price: number;
+  rating: number;
+  image: string;
+}
+
+interface Category {
+  categories: { mainCategory: string[] };
+  headPhones: headPhone[];
+}
+
+async function homeLoader() {
+  let response = await fetch("https://digitalden-backend.onrender.com/");
+  let data: Category = await response.json();
+
+  return response;
+}
+
+export default homeLoader;
