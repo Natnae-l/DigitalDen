e82a6f1e5372546b3e61f0a9234a6d9d2936bac4
Natnael Teklemariam <131717014+Natnae-l@users.noreply.github.com>
2024-03-01 16:20:27 +0300

    config file restored

--- src/components/Nav/components/SearchBar.tsx
+++ src/components/Nav/components/SearchBar.tsx
@@ -1,9 +1,15 @@
 import { Input } from "@mui/base/Input";
+import { IoIosSearch } from "react-icons/io";
 
 function SearchBar() {
   return (
     <div className="dismantle">
-      <Input type="search" placeholder="Search Product" />
+      <Input
+        style={{ display: "flex", alignItems: "center" }}
+        type="search"
+        placeholder="Search Product"
+        endAdornment={<IoIosSearch color="black" size={17} />}
+      />
     </div>
   );
 }
