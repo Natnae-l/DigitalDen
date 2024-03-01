ce1a6c650c4f0c7ab4a33d6e5e8c858a32cf6aba
Natnael Teklemariam <131717014+Natnae-l@users.noreply.github.com>
2024-03-01 21:00:21 +0300

    useEffect hook added a condition to re set the localStorage

--- src/components/Items/Items.tsx
+++ src/components/Items/Items.tsx
@@ -12,10 +12,6 @@ function Items() {
   const { setCart, cart } = useContext(CartContext) as CartContextType;
 
   useEffect(() => {
-    localStorage.setItem("cart", JSON.stringify(cart));
-  }, [cart]);
-
-  useEffect(() => {
     if (cart.length == 0) {
       let storage: headPhone[] = JSON.parse(
         localStorage.getItem("cart") || "[]"
@@ -23,6 +19,11 @@ function Items() {
       setCart([...storage]);
     }
   }, []);
+  useEffect(() => {
+    if (cart.length > 0) {
+      localStorage.setItem("cart", JSON.stringify(cart));
+    }
+  }, [cart]);
 
   if (isLoading) return <DisplaySkeleton />;
   if (error)
